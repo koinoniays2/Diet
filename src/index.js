50bc5cc9bc8e901ba35e0a667398c47e8eed21bc
koinonia2 <koinonia_ys@naver.com>
2024-11-08 16:23:02 +0900

    init (폰트, 리액트쿼리, 라우터 설정)

--- src/index.js
+++ src/index.js
@@ -2,16 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import NotFound from './routes/NotFound';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+// 라우터
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Outlet />, // 자식 컴포넌트들이 "/" 경로에서 <Outlet /> 자리에 렌더링 됨
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "",
+        element: <App />
+      }
+    ]
+  }
+]);
+
+// QueryClient 인스턴스 생성
+const queryClient = new QueryClient(); 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}> {/* 자식 컴포넌트들이 React Query 기능을 사용할 수 있음 */}
+      <RouterProvider router={router} /> {/* 라우팅 설정 */}
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
