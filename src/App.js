02e12b90302b14c889bac07212d99ac16a308645
koinonia2 <koinonia_ys@naver.com>
2024-12-03 17:58:35 +0900

    [update] 메뉴영역감지

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import red from "./assets/close_folder_red.svg";
 import xIcon from "./assets/xicon.svg";
 import setting from "./assets/setting.svg";
@@ -9,8 +9,9 @@ function App() {
   const [folderIconColor, setFolderIconColor] = useState(red);
   const [borderCSS, setBorderCSS] = useState(["border-custom-1", "border-custom-1", "border-custom-1"]);
   const [menuClickOn, setMenuClickOn] = useState([false, false, false]);
-
+  
   const ICON_CONTAINER = `flex-1 flex flex-col items-center justify-center py-2 cursor-pointer`;
+
   const menus = [
     {
       menu : [
@@ -37,10 +38,30 @@ function App() {
       })
     );
   };
+
+  const menuRef = useRef(null); // 메뉴 영역 감지용 ref
+  const handleOutsideClick = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      // 메뉴 바깥을 클릭하면 모든 상태 초기화
+      setBorderCSS(["border-custom-1", "border-custom-1", "border-custom-1"]);
+    }
+  };
+
+  useEffect(() => {
+    // window에 클릭 이벤트 추가
+    window.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      // 컴포넌트 언마운트 시 이벤트 리스너 제거
+      window.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
+
   return (
     <main className={`${baseColor} min-h-screen flex items-center justify-center`}>
       <section className="h-full min-w-[360px] max-w-[480px]">
-        <footer className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full min-w-[360px] max-w-[480px] flex">
+        <footer ref={menuRef} // 메뉴 영역 감지
+        className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full min-w-[360px] max-w-[480px] flex">
           {
             menus.map((item) => (
               item.menu.map((menu, menuIndex) => (
